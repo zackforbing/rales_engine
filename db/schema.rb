152046d4935618e178802cbaed3c28897940c2e7
Zack Forbing <zackforbing@gmail.com>
2016-09-08 08:46:14 -0600

    changed unit_prices to floats (#23)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160905180346) do
+ActiveRecord::Schema.define(version: 20160907195440) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 20160905180346) do
 
   create_table "invoice_items", force: :cascade do |t|
     t.integer  "quantity"
-    t.integer  "unit_price"
+    t.float    "unit_price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "invoice_id"
@@ -46,7 +46,7 @@ ActiveRecord::Schema.define(version: 20160905180346) do
   create_table "items", force: :cascade do |t|
     t.string   "name"
     t.string   "description"
-    t.integer  "unit_price"
+    t.float    "unit_price"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "merchant_id"
