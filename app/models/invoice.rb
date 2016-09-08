36616bac686e0c752b13408b98298bad7064c522
Zack Forbing <zackforbing@gmail.com>
2016-09-08 09:25:44 -0600

    add paid and unpaid methods for Invoice model

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,6 +2,15 @@ class Invoice < ApplicationRecord
   has_many :transactions
   has_many :invoice_items
   has_many :items, through: :invoice_items
+
   belongs_to :customer
   belongs_to :merchant
+
+  def self.paid
+    joins(:transactions).where("result = 'success'")
+  end
+
+  def self.unpaid
+    joins(:transactions).where("result = 'failed'")
+  end
 end
